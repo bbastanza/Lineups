a4fd9aba979898c294a6a2237e05e06ecf782f91
bbastanza <b.bastanza@gmail.com>
2022-12-11 19:54:06 -0500

    add font-awesome and style

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGears } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
   return (
-    <div className="bg-orange-50 w-screen h-screen">
-      <header className="h-16 shadow bg-teal-800 flex items-center justify-between px-4 py-2 text-zinc-50 font-semibold">
+    // <div className="bg-orange-50 w-screen h-screen">
+    <div className="bg-zinc-800 w-screen h-screen">
+      <header className="h-16 shadow bg-teal-800 flex items-center justify-between px-4 py-2 text-zinc-50 font-semibold shadow-zinc-900">
         <div className='flex items-end'>
           <h3 className="text-xl mr-4 font-bold">LineUPs</h3>
           <div className="flex items-end text-sm">
@@ -12,12 +15,15 @@ function App() {
             <p className="nav-link">Page3</p>
           </div>
         </div>
+
+        <FontAwesomeIcon icon={faGears} className="text-xl nav-link" />
       </header>
       <body className="p-4 flex justify-around">
-        <button className="btn btn-norm transition">Normal-Btn</button>
-        <button className="btn btn-light transition">Light-Btn</button>
-        <button className="btn btn-action transition">Action-Btn</button>
-
+        <button className="btn btn-norm transition">Normal</button>
+        <button className="btn btn-light transition">Light</button>
+        <button className="btn btn-action transition">
+          Action
+        </button>
       </body>
     </div>
   );
